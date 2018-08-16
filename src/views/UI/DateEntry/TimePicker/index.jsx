cc18718a8c35e6f2de66b7424053fd210535509b
fridolph <249121486@qq.com>
2018-08-16 14:17:57 +0800

    ui / entry

---
+++ src/views/UI/DateEntry/TimePicker/index.jsx
@@ -0,0 +1,16 @@
+import React, {PureComponent} from 'react'
+import Admin from '../../../../layouts/Admin'
+import BasePage from '../../../../layouts/BasePage'
+import './style.less'
+
+export default class UITimePicker extends PureComponent {
+  render() {
+    return (
+      <Admin>
+        <BasePage className="page-buttons">
+          UITimePicker
+        </BasePage>
+      </Admin>
+    )
+  }
+}
---
+++ src/views/UI/DateEntry/Transfer/index.jsx
@@ -0,0 +1,16 @@
+import React, {PureComponent} from 'react'
+import Admin from '../../../../layouts/Admin'
+import BasePage from '../../../../layouts/BasePage'
+import './style.less'
+
+export default class UITransfer extends PureComponent {
+  render() {
+    return (
+      <Admin>
+        <BasePage className="page-buttons">
+          UITransfer
+        </BasePage>
+      </Admin>
+    )
+  }
+}
